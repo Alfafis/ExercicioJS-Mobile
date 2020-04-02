edeae48860673fed1d3ce42bd9e8d86aeccd1b2e
Alfafis <ctpmmg2009_101@hotmail.com>
2020-04-01 22:53:44 -0300

    exercicio js pre-mobile

---
+++ script.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+
+}
+
+function despertador() {
+   alert('Despertando!')
+}
+
+function despertar() {
+   let input = document.querySelector('#despertador')
+   setTimeout(despertador, input.value)
+}
+
+function tomarAgua() {
+   console.log('Tomar Agua')
+   alert('Hora de tomar Agua!')
+}
+
+function start() {
+   timerId();
+}
+
+let timerId = function () {
+   setInterval(function () {
+      tomarAgua()
+   }, 10000)
+}
+
+
+function loading() {
+   console.log('teste')
+   let loading = document.querySelector('#loading')
+   loading.innerHTML = "<img src='https://media1.tenor.com/images/a388b52cb0b98b71066ce08b9fcc21c5/tenor.gif' />"
+   setTimeout(trocaTexto, 5000)
+}
+
+function trocaTexto() {
+   let loading = document.querySelector('#loading')
+   loading.innerHTML = "Carregado com sucesso"
+
+}
+
+function escrevaNumeros(inicio, fim) {}
